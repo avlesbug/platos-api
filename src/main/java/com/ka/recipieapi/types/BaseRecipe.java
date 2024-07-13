9c947e5ff3217e5e33519f3901653ed932cc4ecf
Kristian Avlesbug <kristian.avlesbug@skatteetaten.no>
2024-07-13 13:44:16 +0200

    Removed totalTime field for prod

--- src/main/java/com/ka/recipieapi/types/BaseRecipe.java
+++ src/main/java/com/ka/recipieapi/types/BaseRecipe.java
@@ -1,6 +1,7 @@
 package com.ka.recipieapi.types;
 
 import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -14,7 +15,6 @@ import lombok.Setter;
 public class BaseRecipe {
     private String name;
     private int portions;
-    private String totalTime;
     private List<String> ingredients;
     private List<String> instructions;
 
@@ -25,13 +25,11 @@ public class BaseRecipe {
     // Parameterized constructor
     @JsonCreator
     public BaseRecipe(@JsonProperty("name") String name,
-        @JsonProperty("portions") int portions,
-        @JsonProperty("totalTime") String totalTime,
+        @JsonProperty("recipeYield") int portions,
         @JsonProperty("ingredients") List<String> ingredients,
         @JsonProperty("instructions") List<String> instructions) {
         this.name = name;
         this.portions = portions;
-        this.totalTime = totalTime;
         this.ingredients = ingredients;
         this.instructions = instructions;
     }
@@ -39,8 +37,7 @@ public class BaseRecipe {
     public RecipeDto toRecipeDto(String imageUrl) {
         return RecipeDto.builder()
             .name(name)
-            .recipeYield(portions)
-            .totalTime(totalTime)
+            .portions(portions)
             .recipeIngredient(ingredients)
             .recipeInstructions(instructions)
             .image(imageUrl)
--- src/main/java/com/ka/recipieapi/types/RecipeDto.java
+++ src/main/java/com/ka/recipieapi/types/RecipeDto.java
@@ -23,29 +23,26 @@ import lombok.ToString;
 public class RecipeDto {
     private String name;
     @JsonDeserialize(using = RecipeYieldDeserializer.class)
-    private Object recipeYield;
+    private Object portions;
     private List<String> recipeIngredient;
     @JsonDeserialize(using = RecipeInstructionsDeserializer.class)
     private List<String> recipeInstructions;
     @JsonDeserialize(using = ImageDeserializer.class)
     private String image;
-    private String totalTime;
 
-    public RecipeDto() {}
+    public RecipeDto() { }
 
     @JsonCreator
     public RecipeDto(@JsonProperty("name") String name,
-        @JsonProperty("portions") @JsonDeserialize(using = RecipeYieldDeserializer.class) Object portions,
+        @JsonProperty("recipeYield") @JsonDeserialize(using = RecipeYieldDeserializer.class) Object recipeYield,
         @JsonProperty("ingredients") List<String> recipeIngredient,
-        @JsonProperty("instructions") @JsonDeserialize(using = RecipeInstructionsDeserializer.class) List<String> recipeInstructions,
-        @JsonProperty("image") @JsonDeserialize(using = ImageDeserializer.class) String image,
-        @JsonProperty("totalTime") String totalTime)
-    {
+        @JsonProperty("instructions") @JsonDeserialize(using = RecipeInstructionsDeserializer.class)
+        List<String> recipeInstructions,
+        @JsonProperty("image") @JsonDeserialize(using = ImageDeserializer.class) String image) {
         this.name = name;
-        this.recipeYield = portions;
+        this.portions = recipeYield;
         this.recipeIngredient = recipeIngredient;
         this.recipeInstructions = recipeInstructions;
         this.image = image;
-        this.totalTime = totalTime;
     }
 }
